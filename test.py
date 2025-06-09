fdbbfec5f8579c8e69b1a19ba490c92e7aeeb9dc
Aashna890 <aashnagaikwad890@gmail.com>
2025-06-09 16:11:28 +0530

    Demo

---
+++ test.py
@@ -0,0 +1,12 @@
+# create our own module in a text editor
+
+# variables to import later
+length = 15
+width = 100
+
+# functions to import later 
+def printInfo(name, age):
+	print("{} is {} years old.".format(name, age))
+	
+	
+	
